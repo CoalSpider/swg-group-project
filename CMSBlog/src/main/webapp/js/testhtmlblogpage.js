ea346927d8445fdf904b4e1f0dfee9c14462225e
Benjamin Lee Norman <copperbeard93@gmail.com>
2018-03-31 11:47:25 -0500

    added test edit page and js

---
+++ CMSBlog/src/main/webapp/js/testhtmlblogpage.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+const basePath = "http://localhost:8080/CMSBlog/";
+
+$(document).ready(function () {
+    loadPageFromServer();
+});
